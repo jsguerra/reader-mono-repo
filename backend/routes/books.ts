30fde19a6a726ac2d3af4f9e4e5ef86981f95bff
Jose Guerra <me@joseguerra.uk>
2024-06-30 11:58:41 +0100

    added routes

--- backend/routes/books.ts
+++ backend/routes/books.ts
@@ -1,6 +1,19 @@
-import { Router } from 'express';
-import { getBooks } from '../controllers/books';
+import { Router } from "express";
+import { getBooks } from "../controllers/books";
 
 export const bookRoutes = Router();
 
-bookRoutes.get('/books', getBooks);
+// Create One book
+bookRoutes.post("/books");
+
+// Get all Books
+bookRoutes.get("/books", getBooks);
+
+// Get One Book
+bookRoutes.get("/book/:bookId");
+
+// Update One Book
+bookRoutes.put("/books/:bookId");
+
+// Delete One Book
+bookRoutes.delete("/books:bookId");
