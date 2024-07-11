7c78d34c0388e767b0b566178aed8bbd6d8cedb8
Jose Guerra <me@joseguerra.uk>
2024-07-11 16:38:09 +0100

    initial commit

---
+++ frontend/src/components/Form/AuthorForm/AuthorForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AuthorForm = () => {
+  return <div>AuthorForm</div>;
+};
+
+export default AuthorForm;
---
+++ frontend/src/components/Form/TagForm/TagForm.tsx
@@ -0,0 +1,44 @@
+import { useRef, useState } from "react";
+import type { FormEvent, FC } from "react";
+import Styles from "../Form.module.css";
+
+interface FormProps {
+  endPoint: string;
+  method: string;
+}
+
+const TagForm: FC<FormProps> = ({ endPoint, method }) => {
+  const [responseMessage, setResponseMessage] = useState("");
+  const ref = useRef<HTMLFormElement>(null);
+
+  async function submit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const response = await fetch(endPoint, {
+      method: method,
+      body: formData,
+    });
+    const data = await response.json();
+    if (data.message) {
+      ref.current?.reset();
+      setResponseMessage(data.message);
+    }
+  }
+
+  return (
+    <form ref={ref} className={Styles.form} onSubmit={submit}>
+      <label>
+        Name
+        <input type="text" id="name" name="name" required />
+      </label>
+      <label>
+        Slug
+        <input type="text" id="slug" name="slug" required />
+      </label>
+      <button>Send</button>
+      {responseMessage && <p>{responseMessage}</p>}
+    </form>
+  );
+};
+
+export default TagForm;
