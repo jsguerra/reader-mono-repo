d05eedcf1db48624c2c78ab61e21218c3a620ab8
Jose Guerra <me@joseguerra.uk>
2024-07-13 22:17:07 +0100

    updated form

--- frontend/src/components/Form/AuthorForm/AuthorForm.tsx
+++ frontend/src/components/Form/AuthorForm/AuthorForm.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import { useRef, useState } from "react";
+import type { FormEvent, FC } from "react";
+import Styles from "../Form.module.css";
 
-const AuthorForm = () => {
-  return <div>AuthorForm</div>;
+interface FormProps {
+  endPoint: string;
+  method: string;
+}
+
+const AuthorForm: FC<FormProps> = ({ endPoint, method }) => {
+  const [responseMessage, setResponseMessage] = useState("");
+  const ref = useRef<HTMLFormElement>(null);
+
+  async function submit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const response = await fetch(endPoint, {
+      method: method,
+      body: formData,
+    });
+    const data = await response.json();
+    if (data.message) {
+      ref.current?.reset();
+      setResponseMessage(data.message);
+    }
+  }
+
+  return (
+    <form ref={ref} className={Styles.form} onSubmit={submit}>
+      <label>
+        Name
+        <input type="text" id="name" name="name" required />
+      </label>
+      <label>
+        Slug
+        <input type="text" id="slug" name="slug" required />
+      </label>
+      <label>
+        Thumbnail
+        <input type="file" id="fileUpload" name="fileUpload" />
+      </label>
+      <button>Submit</button>
+      {responseMessage && <p>{responseMessage}</p>}
+    </form>
+  );
 };
 
 export default AuthorForm;
