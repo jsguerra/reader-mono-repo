e12eae908fe8619a30dbf90353f7eda9342bae60
Jose Guerra <me@joseguerra.uk>
2024-07-15 18:01:45 +0100

    Initial commit

---
+++ frontend/src/components/Form/SearchForm/SearchForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Styles from "./SearchForm.module.css";
+
+const SearchForm = () => {
+  return (
+    <form className={Styles.search}>
+      <input type="search" name="search" id="search" />
+      <button className="btn">Search</button>
+    </form>
+  );
+};
+
+export default SearchForm;
