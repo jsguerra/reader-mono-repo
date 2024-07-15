bf2fd85789fbb567da2eaa037399ae61c60538df
Jose Guerra <me@joseguerra.uk>
2024-07-15 09:35:01 +0100

    small refactor

--- frontend/src/components/Form/AuthorForm/AuthorForm.tsx
+++ frontend/src/components/Form/AuthorForm/AuthorForm.tsx
@@ -11,11 +11,15 @@ const AuthorForm: FC<FormProps> = ({ endPoint, method }) => {
   const [imagePreview, setImagePreview] = useState<
     string | ArrayBuffer | null | undefined
   >(undefined);
+  const [thumbnail, setThumbnail] = useState("");
   const [responseMessage, setResponseMessage] = useState("");
   const ref = useRef<HTMLFormElement>(null);
 
   const handleImageSelect = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
+    if (files && files.length > 0) {
+      setThumbnail(files[0].name);
+    }
 
     if (files && files.length > 0) {
       const reader = new FileReader();
@@ -35,7 +39,7 @@ const AuthorForm: FC<FormProps> = ({ endPoint, method }) => {
   async function submit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
-    // formData.append("thumbnail", files[0].name);
+    formData.append("thumbnail", thumbnail);
     const response = await fetch(endPoint, {
       method: method,
       body: formData,
@@ -43,12 +47,13 @@ const AuthorForm: FC<FormProps> = ({ endPoint, method }) => {
     const data = await response.json();
     if (data.message) {
       ref.current?.reset();
+      setImagePreview(undefined);
       setResponseMessage(data.message);
     }
   }
 
   return (
-    <>
+    <div className={Styles["form-container"]}>
       <form ref={ref} className={Styles.form} onSubmit={submit}>
         <label>
           Name
@@ -67,7 +72,7 @@ const AuthorForm: FC<FormProps> = ({ endPoint, method }) => {
             onChange={handleImageSelect}
           />
         </label>
-        <button>Submit</button>
+        <button className={`btn`}>Submit</button>
         {responseMessage && <p>{responseMessage}</p>}
       </form>
       {imagePreview ? (
@@ -75,7 +80,7 @@ const AuthorForm: FC<FormProps> = ({ endPoint, method }) => {
       ) : (
         <img src="/placeholder.jpg" width="300" alt="" />
       )}
-    </>
+    </div>
   );
 };
 
