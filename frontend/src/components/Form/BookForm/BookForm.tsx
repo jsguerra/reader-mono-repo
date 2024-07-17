826eadf0405790137238f4faa66a9aad6bd3a337
Jose Guerra <me@joseguerra.uk>
2024-07-17 21:49:14 +0100

    updated markup

--- frontend/src/components/Form/BookForm/BookForm.tsx
+++ frontend/src/components/Form/BookForm/BookForm.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import type { ChangeEvent, FormEvent, FC } from "react";
+import Styles from "./BookForm.module.css";
 
 interface AuthorType {
   id: number;
@@ -26,16 +27,27 @@ type FormProps = {
 const BookForm: FC<FormProps> = ({ authors, endPoint, method, tags }) => {
   const [responseMessage, setResponseMessage] = useState("");
   const ref = useRef<HTMLFormElement>(null);
+  const [selectedTags, setSelectedTags] = useState<number[]>([]);
+
+  const handleCheckboxChange = (tagId: number) => {
+    setSelectedTags((prevSelectedTags) => {
+      if (prevSelectedTags.includes(tagId)) {
+        return prevSelectedTags.filter((id) => id !== tagId);
+      } else {
+        return [...prevSelectedTags, tagId];
+      }
+    });
+  };
 
   return (
-    <div className="">
-      <form ref={ref}>
+    <div className={Styles["form-container"]}>
+      <form ref={ref} className={Styles.form}>
         <input
           autoComplete="off"
           type="text"
           id="title"
           name="title"
-          placeholder="Book title"
+          placeholder="Title"
           required
         />
         <input
@@ -43,7 +55,7 @@ const BookForm: FC<FormProps> = ({ authors, endPoint, method, tags }) => {
           type="text"
           id="slug"
           name="slug"
-          placeholder="Book slug"
+          placeholder="Slug"
           required
         />
         <input
@@ -68,10 +80,28 @@ const BookForm: FC<FormProps> = ({ authors, endPoint, method, tags }) => {
               </option>
             ))}
         </select>
-        <label htmlFor="favorite">
+        <label className={Styles.favorite} htmlFor="favorite">
           <input id="favorite" name="favorite" type="checkbox" />{" "}
           <span>Favorite</span>
         </label>
+        {tags.length > 0 && (
+          <div className={Styles.selection}>
+            {tags.map((tag) => {
+              return (
+                <label className={Styles["check-box"]} key={tag.id}>
+                  <input
+                    name="tag"
+                    type="checkbox"
+                    value={tag.id}
+                    checked={selectedTags.includes(tag.id)}
+                    onChange={() => handleCheckboxChange(tag.id)}
+                  />{" "}
+                  <span>{tag.name}</span>
+                </label>
+              );
+            })}
+          </div>
+        )}
         <button className={`btn`}>Submit</button>
         {responseMessage && <p>{responseMessage}</p>}
       </form>
