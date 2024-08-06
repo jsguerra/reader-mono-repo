f0711ebf3db8ad9316d7b4d18932b6f94657fb10
Jose Guerra <me@joseguerra.uk>
2024-08-06 13:48:11 +0100

    initial commit

---
+++ frontend/src/components/AZFilter/AZFilter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface AZFilterProps {
+  letters: string[];
+}
+
+const AZFilter: React.FC<AZFilterProps> = ({ letters }) => {
+  return <div>AZFilter</div>;
+};
+
+export default AZFilter;
